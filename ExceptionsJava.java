5b27a06659a316d57ed1675c3dc8967b6154560f
terravidhal <infiniterraetincelle@gmail.com>
2023-10-08 06:02:40 +0100

    execptionsJava

---
+++ ExceptionsJava.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ExceptionsJava {
+
+    public static void main(String[] args) {
+        ExceptionsJava.testExceptions();
+    }
+
+    public static  void testExceptions() {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        for(int i = 0; i < myList.size(); i++) {
+
+            try{
+                Integer castedValue = (Integer) myList.get(i);
+
+            }catch (ClassCastException e){
+                System.out.println(e + " - " + i + " - " + myList.get(i));
+            }
+        }
+
+       
+
+    }
+
+    
+}
